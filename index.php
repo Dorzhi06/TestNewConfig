027459b87b9382c2b0912b1003744fb17985ee0c
Dorzhi <dorzhi.gombozhapov@yandex.ru>
2021-12-05 04:48:11 +0900

    Start Project

---
+++ index.php
@@ -0,0 +1,20 @@
+<!php 
+    
+!>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <H1>Hello World!</H1>
+    </header>
+    <section id="MainBlock">
+        <H2>This update</H2>
+    </section>
+</body>
+</html>
